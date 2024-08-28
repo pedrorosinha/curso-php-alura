ae7b21dba8400e9bfdb0b959571688a1765cfeaf
pedro felipe <pedrofelipinho@gmail.com>
2024-08-28 09:13:50 -0300

    Criando o programa Screen Match adicionando variáveis de filme

---
+++ screen-match.php
@@ -0,0 +1,10 @@
+<?php 
+    echo "Bem vindo(a) ao Screen Match";
+
+    $nomeFilme = "Top Gun - Maverick";
+    $anoLancamento = 2022;
+    $notaFilme = 8.8;
+    $incluidoNoPlano = true;
+    
+    echo $nomeFilme;
+?>
