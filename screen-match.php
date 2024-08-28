cd6a1d5a7e3fc67e8df6f179404de712b340e5a7
pedro felipe <pedrofelipinho@gmail.com>
2024-08-28 10:38:50 -0300

    Conhecendo o match, expressão condicional

--- screen-match.php
+++ screen-match.php
@@ -2,6 +2,9 @@
     echo "Bem vindo(a) ao Screen Match\n";
 
     $nomeFilme = "Top Gun - Maverick";
+    // $nomeFilme = "Divertidamente 2";
+    // $nomeFilme = "Thor: Ragnarok";
+
     $anoLancamento = $argv[1] ?? 2022;
 
     $somaNotas = 9;
@@ -22,11 +25,20 @@
     echo "Ano de lançamento: $anoLancamento \n";
 
     if ($anoLancamento > 2022) {
-        echo "Esse filme é um lançamento";      
+        echo "Esse filme é um lançamento\n";      
     } elseif ($anoLancamento > 2020 && $anoLancamento <= 2022) {
-        echo "Esse filme ainda é novo";
+        echo "Esse filme ainda é novo\n";
     }
     else {
-        echo "Esse filme não é um lançamento";
+        echo "Esse filme não é um lançamento\n";
     }
+
+    $genero = match ($nomeFilme) {
+        "Top Gun - Maverick" => "Ação",
+        "Thor: Ragnarok" => "Super-herói",
+        "Divertidamente 2" => "Animação",
+        default => "Gênero desconhecido"
+    };
+
+    echo "O gênero do filme $nomeFilme é: $genero\n";
 ?>
