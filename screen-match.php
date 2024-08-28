af5daed82e7a8ad805e8014c5a8038ef732df0ce
pedro felipe <pedrofelipinho@gmail.com>
2024-08-28 09:30:03 -0300

    Utilizando operadores

--- screen-match.php
+++ screen-match.php
@@ -1,10 +1,19 @@
 <?php 
-    echo "Bem vindo(a) ao Screen Match";
+    echo "Bem vindo(a) ao Screen Match\n";
 
     $nomeFilme = "Top Gun - Maverick";
     $anoLancamento = 2022;
-    $notaFilme = 8.8;
-    $incluidoNoPlano = true;
-    
-    echo $nomeFilme;
+
+    $somaNotas = 9;
+    $somaNotas += 6;
+    $somaNotas += 8;
+    $somaNotas += 7.5;
+    $somaNotas += 5;
+
+    $notaFilme = $somaNotas / 5;
+    $planoPrime = true;
+
+    $incluidoNoPlano = $planoPrime || $anoLancamento < 2020;
+
+    echo $notaFilme;
 ?>
